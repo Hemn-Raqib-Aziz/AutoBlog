7913b86defb4e815b874af6bf962b12f65ab1b7d
Hemn-Raqib-Aziz <172915789+Hemn-Raqib-Aziz@users.noreply.github.com>
2025-12-10 22:27:23 +0300

    update cron job to 10:33

--- back-end/src/services/articleJob.js
+++ back-end/src/services/articleJob.js
@@ -19,7 +19,7 @@ const createScheduledRandomBlog = async () => {
 };
 
 export const startArticleCron = () => {
-  cron.schedule("0 3 * * *", async () => {
+  cron.schedule("33 32 * * *", async () => {
     console.log("Running daily random article cron...");
     await createScheduledRandomBlog();
   });
